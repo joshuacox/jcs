e2d66d3fa860cc7dd956378ee72169800a3943d5
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-11-22 20:43:21 -0600

    bootstrap

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # jcs
+
+## Installation
+
+### oneliner
+
+```
+curl -sL https://raw.githubusercontent.com/joshuacox/jcs/refs/heads/master/bootstrap.sh | bash
+```
---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# namgen
+curl https://raw.githubusercontent.com/joshuacox/namgen/refs/heads/main/scripts/install.sh | sh
+# vv
+curl -sL https://raw.githubusercontent.com/joshuacox/vv/refs/heads/master/bootstrapvv.sh | bash
+# loopster
+curl -sL https://raw.githubusercontent.com/joshuacox/loopster/refs/heads/master/bootstrap.sh | bash
+# roustabout
+curl -sL https://raw.githubusercontent.com/joshuacox/roustabout/refs/heads/master/bootstraproustabout.sh | bash
+# Nu 
+curl -sL https://raw.githubusercontent.com/joshuacox/Nu/refs/heads/main/bootstrapNu.sh | bash
+# passgen
+curl https://raw.githubusercontent.com/joshuacox/passgen/master/bootstrappassgen.sh|sh
+# wgu
+curl -sL https://raw.githubusercontent.com/joshuacox/wgu/refs/heads/master/bootstrap.sh | bash

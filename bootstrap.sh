12d51fc004084462e02237adfb6d2de50cc29791
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-11-22 20:57:58 -0600

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,3 +13,13 @@ curl -sL https://raw.githubusercontent.com/joshuacox/Nu/refs/heads/main/bootstra
 curl https://raw.githubusercontent.com/joshuacox/passgen/master/bootstrappassgen.sh|sh
 # wgu
 curl -sL https://raw.githubusercontent.com/joshuacox/wgu/refs/heads/main/bootstrap.sh | bash
+# bomsaway
+curl https://raw.githubusercontent.com/joshuacox/bomsaway/master/bootstrapbomsaway.sh |sudo bash
+# SSShutdown
+curl -sL https://git.io/ssshutdown | bash
+# Swappy
+curl https://raw.githubusercontent.com/joshuacox/swappy/master/bootstrap | bash
+# wg-init
+curl https://raw.githubusercontent.com/joshuacox/wg-init/main/bootstrap | sh
+# Clip
+curl -sL https://git.io/clipinstall | bash
